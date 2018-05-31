038ee4a4f844e70da3ebd0c58a5f0bdf781ba8b3
sheeshka <sheeshka@ya.ru>
2018-06-01 01:32:16 +0400

    header2

--- resources/views/layouts/header2.blade.php
+++ resources/views/layouts/header2.blade.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 01.06.2018
- * Time: 0:27
- */
+<header class="header header-mobile js-header-bar-mobile d-md-none">
+    <div class="header-bar">
+        <div class="header-bar-logo">
+            <a href="index.html">
+                <img src="images/icons/logo.png" alt="Ora">
+            </a>
+        </div>
+        <div class="header-bar-menu">
+            <button class="navbar-toggler hamburger" type="button" id="js-header-toggle">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+            </button>
+        </div>
+    </div>
+    <nav class="navbar-mobile navbar-primary">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#intro">ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#services">SERVICES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#resume">RESUME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#projects">WORKS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#blog">BLOG</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#testimonials">TESTIMONIALS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">CONTACT</a>
+            </li>
+        </ul>
+    </nav>
+</header>
