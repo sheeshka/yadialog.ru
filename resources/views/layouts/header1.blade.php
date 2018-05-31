65682bdddfb8df1dab055a470dad10d5aa9f610e
sheeshka <shabalinka@image-mg.com>
2018-05-31 19:07:15 +0300

    incl

---
+++ resources/views/layouts/header1.blade.php
@@ -0,0 +1,38 @@
+<header class="header header-primary header-fixed js-header-bar d-md-block d-none">
+    <div class="container-fluid">
+        <div class="section-inner header-bar">
+            <div class="header-bar-logo">
+                <a href="index.html">
+                    <img src="images/icons/logo.png" alt="Ora">
+                </a>
+            </div>
+            <div class="header-bar-menu">
+                <nav class="navbar-primary">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#intro">ABOUT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#services">SERVICES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#resume">RESUME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#projects">WORKS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#blog">BLOG</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#testimonials">TESTIMONIALS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">CONTACT</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
