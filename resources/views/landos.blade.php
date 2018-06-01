c1cc07de3cddef497e379c662a7d97303dda6014
sheeshka <shabalinka@image-mg.com>
2018-06-01 12:14:00 +0300

    top_nav

--- resources/views/landos.blade.php
+++ resources/views/landos.blade.php
@@ -41,44 +41,7 @@
 
 
 <!-- header-->
-<header class="header header-primary header-fixed js-header-bar d-md-block d-none">
-    <div class="container-fluid">
-        <div class="section-inner header-bar">
-            <div class="header-bar-logo">
-                <a href="index.html">
-                    <img src="images/icons/logo.png" alt="Ora">
-                </a>
-            </div>
-            <div class="header-bar-menu">
-                <nav class="navbar-primary">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#intro">ABOUT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services">SERVICES</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#resume">RESUME</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#projects">WORKS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#blog">BLOG</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#testimonials">TESTIMONIALS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">CONTACT</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-</header>
+    @include('top_nav')
 <header class="header header-mobile js-header-bar-mobile d-md-none">
     <div class="header-bar">
         <div class="header-bar-logo">
---
+++ resources/views/layouts/top_nav.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sheeshka
+ * Date: 01.06.2018
+ * Time: 12:13
+ */
