6e39b712ca9fe0baff769428950515f9153c8bd1
sheeshka <shabalinka@image-mg.com>
2018-06-08 19:56:43 +0300

    meta ya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="robots" content="index, follow">
+    <meta name="yandex-verification" content="ec01a266139f5805" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="css/glyad.css" type="text/css">
