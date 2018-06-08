7418fc1b4d8d96c190c1d845a8fb491bbfa49f3a
sheeshka <shabalinka@image-mg.com>
2018-06-08 17:59:20 +0300

    главная

---
+++ resources/views/home.blade.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/mybootstrap.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/glyad.css" type="text/css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+<div class="container-fluid">
+    <div id="section1" class="col-12 bg">
+        <div class="container">
+            <div class="row align-items-center navbarr">
+                <div id="logo" class="col-lg-5">YaDialog</div>
+                <div class="col-lg-7">
+                    <ul class="nav">
+                        <li class="navmen"><a href="#">Пункт меню 1</a></li>
+                        <li class="navmen"><a href="#">Пункт меню 2</a></li>
+                        <li class="navmen"><a href="#">Пункт меню 3</a></li>
+                        <li class="navmen"><a href="#">Пункт меню 4</a></li>
+                        <li class="navmen"><a href="#">Пункт меню 5</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <h1>YaDialog - крупная платформа для создания Telegram бота и Навыков Алисы.</h1>
+            </div>
+            <div class="row">
+                <p id="parag">У нас вы можете легко сделать заказ, и уже через неделю взаимодействовать с клиентами, выполняя поставленные задачи.
+                    У нас работают профессиональные маркетологи, которые помогут в разработке алгоритма общения бота с клиентомУ нас работает
+                    23 программиста и 7 тестировщиков, которые готовы для вас выполнить самые сложные решения в минимальные срокиНаши аккаунт
+                    менеджеры всегда оперативно реагируют на все запросы и следят за ходом </p>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                    <div id="botton">
+                        <p id="tbotton">Начать работу</p>
+                        <div id="lbotton"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="section2" class="col-12 bg">
+        <div class="container">
+            <div class="row align-items-center navbarr">
+                <div class="col-10 offset-1">
+                    <h2>Telegram бот – современный способ взаимодействия с клиентом. </h2>
+                    <div class="s2line">
+                    </div>
+                    <div class="s2pt">
+                        <p>Telegram бот – современный способ взаимодействия с клиентом.</p>
+                        <p>современный способ взаимодействия с клиентом</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2 offset-1">
+                    <div id="blok1">
+
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-1">
+                    <div id="blok2">
+
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-1">
+                    <div id="blok3">
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div id="gif1" class="col-lg-10 offset-1" >
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="section3" class="col-12 bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="s3tyt">
+                        <h2>Таблица цен на услуги</h2>
+                    </div>
+                    <div class="s3line"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-10 offset-1">
+                    <p id="s3pt">Длинный длинный длинный длинный длинный длинный длинный длинный текст на первой строке</p>
+                </div>
+            </div>
+            <div class="row">
+                <div id="price1" class="col-lg col-md-6 price">
+
+                </div>
+                <div id="price2" class="col-lg col-md-6 price">
+
+                </div>
+                <div id="price3" class="col-lg col-md price">
+
+                </div>
+                <div id="price4" class="col-lg col-md price">
+
+                </div>
+                <div id="price5" class="col-lg col-md price">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="section4" class="col-12 bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <div class="s4tyt">
+                        <h2>Этапы работ: </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="s4line"></div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-lg-6">
+                    <div class="s4t">
+                        <p>Какой-то текст с идеей </br> для краоты</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg s4et">
+
+                </div>
+                <div class="col-lg s4et">
+
+                </div>
+                <div class="col-lg s4et">
+
+                </div>
+                <div class="col-lg s4et">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="section5" class="col-12 bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <div class="s5tyt">
+                        <h2>Наши партнёры и клиенты</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="s5line"></div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-lg-6">
+                    <div class="s5t">
+                        <p>Какой-то текст с идеей текста</br> для краоты</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col">
+                            <div id="s5b1" class="kn">
+                                <p>Текст</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div id="s5b2" class="kn">
+                                <p>Текст</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div id="s5b3" class="kn">
+                                <p>Текст</p>
+                            </div></div>
+                        <div class="col">
+                            <div id="s5b4" class="kn">
+                                <p>Текст</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="s5pr">
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="s5pr">
+
+                    </div>
+                </div>
+                <div class="w-100"></div>
+                <div class="col">
+                    <div class="s5pr">
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="s5pr">
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                    <div class="s5btn">
+                        <p>Текст кнопки</p>
+                        <div class="s5lineb"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                    <div class="s5part">
+                        <p>Наши партнёры:</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1">
+                    <div class="s5back"></div>
+                </div>
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                        <div class="col-lg">
+                            <div class="s5prt"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1"><div class="s5next"></div></div>
+            </div>
+        </div>
+    </div>
+    <div id="section6" class="col-12 bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <div class="s6h">
+                        <h2>Как с Нами можно связаться:</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="s6line"></div>
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                    <p>Текст как всегда ниже</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
+                    <div class="s6sv" id="s6sv1">№</div>
+                </div>
+                <div class="col-lg">
+                    <div class="s6sv" id="s6sv2">№</div>
+                </div>
+                <div class="w-100"></div>
+                <div class="col-lg">
+                    <div class="s6sv" id="s6sv3">№</div>
+                </div>
+                <div class="col-lg">
+                    <div class="s6sv" id="s6sv4">№</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="s6tn">
+                        <p>Всегда рады любому общению с Вами!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="section7" class="col-12 bg">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="s7t">
+                        <p>Подвал с контактами и дублированием меню или информации</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="section8" class="col-12 bg">
+
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<body>
+<html>
