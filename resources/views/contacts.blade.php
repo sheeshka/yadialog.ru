bccf0d0d79119a88d152e341facbe1edfbb3671b
sheeshka <shabalinka@image-mg.com>
2018-06-09 11:05:30 +0300

    view

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sheeshka
+ * Date: 09.06.2018
+ * Time: 11:04
+ */
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('', function () {
+Route::get('/', function () {
     return view('home');
 });
 
